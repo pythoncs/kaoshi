095f34c0241c92361c35875a8a346f2cffb4896f
pythoncs <1640368651@qq.com>
2018-03-20 18:22:52 +0800

    考试

--- cai.py
+++ cai.py
@@ -4,13 +4,15 @@ class CuiShu_cai(object):
 
 	def __init__(self):
 
-		self.computer = random.randint(0,99)
+		#self.computer = random.randint(0,99)
 		self.again = 1
 		self.count = 0
 		self.count_list = []
+		self.sum = 0
 	def CuiShu_panduan(self):
 
 		while True:
+			self.computer = random.randint(0,99)
 			self.count += 1
 			self.person = int(input("请输入一个整数: "))
 
@@ -21,10 +23,11 @@ class CuiShu_cai(object):
 					self.again += 1
 					self.count_list.append(self.count)
 					self.count = 0
+					
 				if choice == 2:
 					self.count_list.append(self.count)
 					break
-					self.Cuishu_jisuan()
+					
 			elif self.person > self.computer:
 				print("你猜的数字有点大")
 
@@ -39,14 +42,14 @@ class CuiShu_cai(object):
 		for num in self.count_list:
 			
 			print("每次各猜了" + str(num) + "次.")
-			num += num
+			self.sum += num
 		#平均猜中的次数
-		if self.again == 1:
-			sum_number = (num / 2) / self.again
-			print("平均每次猜了" + str(sum_number) + "次才猜中.")
-		elif self.again > 1:
-			sum_number = num / self.again
-			print("平均每次猜了" + str(sum_number) + "次才猜中.")
+		#if self.again == 1:
+			#sum_number = (num / 2) / self.again
+			#print("平均每次猜了" + str(sum_number) + "次才猜中.")
+		#elif self.again > 1:
+		sum_number = self.sum / self.again
+		print("平均每次猜了" + str(sum_number) + "次才猜中.")
 
 CuiShu = CuiShu_cai()
 CuiShu.CuiShu_panduan()
